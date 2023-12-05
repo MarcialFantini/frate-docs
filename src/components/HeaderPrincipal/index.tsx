0fe269e09999bb23626166cb242f27c38dcaedc9
MarcialFantini <marcialfantini@gmail.com>
2023-12-05 14:18:38 -0300

    updates

--- src/components/HeaderPrincipal/index.tsx
+++ src/components/HeaderPrincipal/index.tsx
@@ -4,6 +4,7 @@ export function HeaderPrincipal() {
   return (
     <header className=" min-h-[90vh] w-full relative overflow-hidden flex flex-col items-center justify-center">
       <ImagesHeader></ImagesHeader>
+
       <div className=" w-[80%]">
         <h2 className=" text-[5rem]">Frate</h2>
         <p className=" text-[1.2rem]">
--- src/components/ImagesHeader/index.tsx
+++ src/components/ImagesHeader/index.tsx
@@ -1,80 +1,77 @@
 "use client";
-
 import styled from "./styled.module.css";
-import Image from "next/image";
+
 import srcDefault1 from "../../../public/images/00.jpg";
 import srcDefault2 from "../../../public/images/10.jpg";
 import srcDefault3 from "../../../public/images/01.jpg";
 import srcDefault4 from "../../../public/images/11.jpg";
-
-import { Dispatch, SetStateAction, useState } from "react";
+import Image from "next/image";
+import { Suspense, useState } from "react";
 
 export function ImagesHeader() {
   const [picture1, setPicture1] = useState(false);
   const [picture2, setPicture2] = useState(false);
   const [picture3, setPicture3] = useState(false);
   const [picture4, setPicture4] = useState(false);
-  const [complete, setComplete] = useState(false);
 
-  const handlerActive = (callback: Dispatch<SetStateAction<boolean>>) => () => {
-    callback(true);
-    setTimeout(() => {
-      setComplete(true);
-    }, 500);
+  const handler1 = () => setPicture1(true);
+  const handler2 = () => setPicture2(true);
+  const handler3 = () => setPicture3(true);
+  const handler4 = () => {
+    setPicture4(true);
   };
 
   return (
     <div className={styled.containerHeader}>
       {/* Cuadrante 1 */}
-      <picture
-        style={{ transform: complete ? "" : "translate(-100%,-100%)" }}
-        className={styled.picture1}
-      >
-        <Image
-          onLoad={handlerActive(setPicture1)}
-          className={styled.img}
-          src={srcDefault1}
-          alt="hola"
-        />
+      <picture className={picture1 ? "" : styled.picture1}>
+        <Suspense>
+          <Image
+            onLoad={handler1}
+            className={styled.img}
+            src={srcDefault1}
+            alt="hola"
+          />
+        </Suspense>
       </picture>
 
       {/* Cuadrante 2 */}
-      <picture
-        style={{ transform: complete ? "" : "translate(100%,-100%)" }}
-        className={styled.picture2}
-      >
-        <Image
-          onLoad={handlerActive(setPicture2)}
-          className={styled.img}
-          src={srcDefault2}
-          alt="hola"
-        />
+      <picture className={picture2 ? "" : styled.picture2}>
+        {" "}
+        <Suspense>
+          <Image
+            onLoad={handler2}
+            className={styled.img}
+            src={srcDefault2}
+            alt="hola"
+          />{" "}
+        </Suspense>
       </picture>
 
       {/* Cuadrante 3 */}
-      <picture
-        style={{ transform: complete ? "" : "translate(-100%,100%)" }}
-        className={styled.picture3}
-      >
-        <Image
-          onLoad={handlerActive(setPicture3)}
-          className={styled.img}
-          src={srcDefault3}
-          alt="hola"
-        />
+      <picture className={picture3 ? "" : styled.picture3}>
+        {" "}
+        <Suspense>
+          <Image
+            onLoad={handler3}
+            className={styled.img}
+            src={srcDefault3}
+            alt="hola"
+          />{" "}
+        </Suspense>
       </picture>
 
       {/* Cuadrante 4 */}
-      <picture
-        style={{ transform: complete ? "" : "translate(100%,100%)" }}
-        className={styled.picture4}
-      >
-        <Image
-          onLoad={handlerActive(setPicture4)}
-          className={styled.img}
-          src={srcDefault4}
-          alt="hola"
-        />
+      <picture className={picture4 ? "" : styled.picture4}>
+        {" "}
+        <Suspense>
+          <Image
+            onLoad={handler4}
+            className={styled.img}
+            src={srcDefault4}
+            alt="hola"
+          />{" "}
+        </Suspense>
       </picture>
     </div>
   );
